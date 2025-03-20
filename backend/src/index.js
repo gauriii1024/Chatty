a832abaebf336cc5784d050c529946fe3d8bd56e
gauriii1024 <gauri10022004@gmail.com>
2025-03-20 15:25:51 +0530

    Login Page done

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import {connectDB} from "./lib/db.js"
 import authRoutes from "./routes/auth.route.js"
 import cookieParser from "cookie-parser"
 import messageRoutes from "./routes/message.route.js"
+import cors from "cors";
 
 dotenv.config();
 
@@ -12,7 +13,13 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(cors(
+    {
+        origin: "http://localhost:5173",
+        credentials: true,
+    }
+))
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
